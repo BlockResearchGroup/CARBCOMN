a3fcc829ca87a3aaf33f65be3f3dbd1ec7364a28
tomvanmele <tom.v.mele@gmail.com>
2025-01-22 01:24:28 +0100

    update rhino venv

--- rhino_venv.py
+++ rhino_venv.py
@@ -1,10 +1,7 @@
 #! python3
 # venv: carbcomn
-# r: compas_fd, compas_dr, compas_tna, compas_model, compas_masonry
+# r: compas
 
 import compas
-import compas_fd
-import compas_dr
-import compas_tna
-import compas_model
-import compas_masonry
+
+print(compas.__version__)
